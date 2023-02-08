83f708a684eed5d7227472f79ded5678a28ba77a
fukamachi1063 <78638383+fukamachi1063@users.noreply.github.com>
2023-02-08 23:23:01 +0900

    その他の画面＆共通化

--- Views/search.php
+++ Views/search.php
@@ -1,37 +1,74 @@
 <?php
+
 //設定関連を読み込む
+
 include_once('../config.php');
+
 //便利な関数を読み込む
+
 include_once('../util.php');
 
+
+
  
+
 ///////////////////////////////////////
+
 // ツイート一覧
+
 ///////////////////////////////////////
+
 $view_tweets = [
+
     [
+
         'user_id' => 1,
+
         'user_name' => 'taro',
+
         'user_nickname' => '太郎',
+
         'user_image_name' => 'sample-person.jpg',
+
         'tweet_body' => '今プログラミングをしています。',
+
         'tweet_image_name' => null,
+
         'tweet_created_at' => '2023-02-02 14:00:00',
+
         'like_id' => null,
+
         'like_count' => 0,
+
     ],
+
     [
+
         'user_id' => 2,
+
         'user_name' => 'jiro',
+
         'user_nickname' => '次郎',
+
         'user_image_name' => null,
+
         'tweet_body' => 'コワーキングスペースをオープンしました！',
+
         'tweet_image_name' => 'sample-post.jpg',
+
         'tweet_created_at' => '2023-02-02 14:00:00',
+
         'like_id' => 1,
+
         'like_count' => 1,
+
     ]
+
 ];
+
+
+
+
 
 
 
@@ -39,59 +76,91 @@ $view_tweets = [
 
 
 
+
+
+
+
 <!DOCTYPE html>
+
 <html lang="ja">
+
  
+
 <head>
+
     <?php include_once('../Views/common/head.php'); ?>
 
-    <title>ホーム画面 / Twitterクローン</title>
-    <meta name="description" content="ホーム画面です">
+
+
+    <title>検索画面 / Twitterクローン</title>
+
+    <meta name="description" content="検索画面です">
+
 </head>
+
  
-<body class="home">
+
+<body class="home search text-center">
+
     <div class="container">
+
     <?php include_once('../Views/common/side.php'); ?>
+
         <div class="main">
+
             <div class="main-header">
-                <h1>ホーム</h1>
+
+                <h1>検索</h1>
+
             </div>
+
+            <!--検索エリア-->
+            <form action="search.php" method="get">
+                <div class="search-area">
+                    <input type="text" class="form-control" placeholder="キーワード検索" name="keyword" value="">
+                    <button type="submit" class="btn">検索</button>
+                </div>
+            </form>
+
  
-            <!-- つぶやき投稿エリア -->
-            <div class="tweet-post">
-                <div class="my-icon">
-                    <img src="<?php echo HOME_URL;?>Views/img_uploaded/user/sample-person.jpg" alt="">
-                </div>
-                <div class="input-area">
-                    <form action="post.php" method="post" enctype="multipart/form-data">
-                        <textarea name="body" placeholder="いまどうしてる？" maxlength="140"></textarea>
-                        <div class="bottom-area">
-                            <div class="mb-0">
-                                <input type="file" name="image" class="form-control form-control-sm">
-                            </div>
-                            <button class="btn" type="submit">つぶやく</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+
 
             <!--仕切りエリア-->
+
             <div class= "ditch"></div>
+
  
 
+
+
             <!--つぶやき一覧エリア-->
+
             <?php if (empty($view_tweets)) : ?>
+
                 <p class= "p-3">ツイートがありません</p>
+
             <?php else: ?>
+
             <div class= "tweet-list">
+
                 <?php foreach($view_tweets as $view_tweet):?>
+
                     <?php include('../Views/common/tweet.php'); ?>
+
                 <?php endforeach;?>
+
             </div>
+
             <?php endif; ?>
+
         </div>
+
     </div>
+
     <?php include_once('../Views/common/foot.php'); ?>
+
 </body>
+
  
+
 </html>
